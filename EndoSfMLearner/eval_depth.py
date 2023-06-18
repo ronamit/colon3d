995b769ba78ccb46107fa7d4a3fb3a1043642c10
ronamit <ronamit@campus.technion.ac.il>
2023-06-18 15:53:36 +0000

    refact

--- EndoSfMLearner/eval_depth.py
+++ EndoSfMLearner/eval_depth.py
@@ -1,15 +1,16 @@
 import argparse
+import os
+from pathlib import Path
+
 import cv2
 import matplotlib as mpl
-import matplotlib.cm as cm
 import numpy as np
-import os
+from matplotlib import cm
 from tqdm import tqdm
-from path import Path
 
 ################### Options ######################
 parser = argparse.ArgumentParser(description="NYUv2 Depth options")
-parser.add_argument("--dataset", required=True, help="kitti or nyu", choices=['nyu', 'kitti'], type=str)
+parser.add_argument("--dataset", required=True, help="kitti or nyu", choices=["nyu", "kitti"], type=str)
 parser.add_argument("--pred_depth", required=True, help="depth predictions npy", type=str)
 parser.add_argument("--gt_depth", required=True, help="gt depth nyu for nyu or folder for kitti", type=str)
 parser.add_argument("--vis_dir", help="result directory for saving visualization", type=str)
@@ -37,23 +38,24 @@ def compute_depth_errors(gt, pred):
     """
     thresh = np.maximum((gt / pred), (pred / gt))
     a1 = (thresh < 1.25).mean()
-    a2 = (thresh < 1.25 ** 2).mean()
-    a3 = (thresh < 1.25 ** 3).mean()
+    a2 = (thresh < 1.25**2).mean()
+    a3 = (thresh < 1.25**3).mean()
 
     rmse = (gt - pred) ** 2
     rmse = np.sqrt(rmse.mean())
     rmse_log = (np.log(gt) - np.log(pred)) ** 2
     rmse_log = np.sqrt(rmse_log.mean())
 
-    log10 = np.mean(np.abs((np.log10(gt) - np.log10(pred))))
+    log10 = np.mean(np.abs(np.log10(gt) - np.log10(pred)))
 
     abs_rel = np.mean(np.abs(gt - pred) / gt)
     sq_rel = np.mean(((gt - pred) ** 2) / gt)
 
-    if args.dataset == 'nyu':
+    if args.dataset == "nyu":
         return abs_rel, log10, rmse, a1, a2, a3
-    elif args.dataset == 'kitti':
+    elif args.dataset == "kitti":
         return abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3
+    return None
 
 
 def depth_visualizer(data):
@@ -67,7 +69,7 @@ def depth_visualizer(data):
     inv_depth = 1 / (data + 1e-6)
     vmax = np.percentile(inv_depth, 95)
     normalizer = mpl.colors.Normalize(vmin=inv_depth.min(), vmax=vmax)
-    mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
+    mapper = cm.ScalarMappable(norm=normalizer, cmap="magma")
     vis_data = (mapper.to_rgba(inv_depth)[:, :, :3] * 255).astype(np.uint8)
     return vis_data
 
@@ -85,7 +87,7 @@ def depth_pair_visualizer(pred, gt):
 
     vmax = np.percentile(inv_gt, 95)
     normalizer = mpl.colors.Normalize(vmin=inv_gt.min(), vmax=vmax)
-    mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
+    mapper = cm.ScalarMappable(norm=normalizer, cmap="magma")
 
     vis_pred = (mapper.to_rgba(inv_pred)[:, :, :3] * 255).astype(np.uint8)
     vis_gt = (mapper.to_rgba(inv_gt)[:, :, :3] * 255).astype(np.uint8)
@@ -93,15 +95,14 @@ def depth_pair_visualizer(pred, gt):
     return vis_pred, vis_gt
 
 
-class DepthEvalEigen():
+class DepthEvalEigen:
     def __init__(self):
-
         self.min_depth = 1e-3
 
-        if args.dataset == 'nyu':
-            self.max_depth = 10.
-        elif args.dataset == 'kitti':
-            self.max_depth = 80.
+        if args.dataset == "nyu":
+            self.max_depth = 10.0
+        elif args.dataset == "kitti":
+            self.max_depth = 80.0
 
     def main(self):
         pred_depths = []
@@ -111,9 +112,9 @@ class DepthEvalEigen():
         pred_depths = np.load(os.path.join(args.pred_depth))
 
         """ Evaluation """
-        if args.dataset == 'nyu':
+        if args.dataset == "nyu":
             gt_depths = np.load(args.gt_depth)
-        elif args.dataset == 'kitti':
+        elif args.dataset == "kitti":
             gt_depths = []
             for gt_f in sorted(Path(args.gt_depth).files("*.npy")):
                 gt_depths.append(np.load(gt_f))
@@ -123,10 +124,10 @@ class DepthEvalEigen():
         """ Save result """
         # create folder for visualization result
         if args.vis_dir:
-            save_folder = Path(args.vis_dir)/'vis_depth'
+            save_folder = Path(args.vis_dir) / "vis_depth"
             mkdir_if_not_exists(save_folder)
 
-            image_paths = sorted(Path(args.img_dir).files('*.png'))
+            image_paths = sorted(Path(args.img_dir).files("*.png"))
 
             for i in tqdm(range(len(pred_depths))):
                 # reading image
@@ -136,24 +137,24 @@ class DepthEvalEigen():
                 h, w, _ = img.shape
 
                 cat_img = 0
-                if args.dataset == 'nyu':
-                    cat_img = np.zeros((h, 3*w, 3))
+                if args.dataset == "nyu":
+                    cat_img = np.zeros((h, 3 * w, 3))
                     cat_img[:, :w] = img
                     pred = pred_depths[i]
                     gt = gt_depths[i]
                     vis_pred, vis_gt = depth_pair_visualizer(pred, gt)
-                    cat_img[:, w:2*w] = vis_pred
-                    cat_img[:, 2*w:3*w] = vis_gt
-                elif args.dataset == 'kitti':
-                    cat_img = np.zeros((2*h, w, 3))
+                    cat_img[:, w : 2 * w] = vis_pred
+                    cat_img[:, 2 * w : 3 * w] = vis_gt
+                elif args.dataset == "kitti":
+                    cat_img = np.zeros((2 * h, w, 3))
                     cat_img[:h] = img
                     pred = pred_depths[i]
                     vis_pred = depth_visualizer(pred)
-                    cat_img[h:2*h, :] = vis_pred
+                    cat_img[h : 2 * h, :] = vis_pred
 
                 # save image
                 cat_img = cat_img.astype(np.uint8)
-                png_path = os.path.join(save_folder, "{:04}.png".format(i))
+                png_path = os.path.join(save_folder, f"{i:04}.png")
                 cv2.imwrite(png_path, cv2.cvtColor(cat_img, cv2.COLOR_RGB2BGR))
 
     def evaluate_depth(self, gt_depths, pred_depths, eval_mono=True):
@@ -181,12 +182,13 @@ class DepthEvalEigen():
                 pred_depth = 1 / (pred_inv_depth + 1e-6)
 
                 mask = np.logical_and(gt_depth > self.min_depth, gt_depth < self.max_depth)
-                if args.dataset == 'kitti':
+                if args.dataset == "kitti":
                     gt_height, gt_width = gt_depth.shape
-                    crop = np.array([0.40810811 * gt_height,  0.99189189 * gt_height,
-                                     0.03594771 * gt_width,   0.96405229 * gt_width]).astype(np.int32)
+                    crop = np.array(
+                        [0.40810811 * gt_height, 0.99189189 * gt_height, 0.03594771 * gt_width, 0.96405229 * gt_width],
+                    ).astype(np.int32)
                     crop_mask = np.zeros(mask.shape)
-                    crop_mask[crop[0]:crop[1], crop[2]:crop[3]] = 1
+                    crop_mask[crop[0] : crop[1], crop[2] : crop[3]] = 1
                     mask = np.logical_and(mask, crop_mask)
 
                 val_pred_depth = pred_depth[mask]
@@ -210,17 +212,17 @@ class DepthEvalEigen():
         if eval_mono:
             ratios = np.array(ratios)
             med = np.median(ratios)
-            print(" Scaling ratios | med: {:0.3f} | std: {:0.3f}".format(med, np.std(ratios / med)))
-            print(" Scaling ratios | mean: {:0.3f} +- std: {:0.3f}".format(np.mean(ratios), np.std(ratios)))
+            print(f" Scaling ratios | med: {med:0.3f} | std: {np.std(ratios / med):0.3f}")
+            print(f" Scaling ratios | mean: {np.mean(ratios):0.3f} +- std: {np.std(ratios):0.3f}")
             if args.ratio_name:
-                np.savetxt(args.ratio_name, ratios, fmt='%.4f')
+                np.savetxt(args.ratio_name, ratios, fmt="%.4f")
 
         mean_errors = np.array(errors).mean(0)
 
-        if args.dataset == 'nyu':
+        if args.dataset == "nyu":
             print("\n  " + ("{:>8} | " * 6).format("abs_rel", "log10", "rmse", "a1", "a2", "a3"))
             print(("&{: 8.3f}  " * 6).format(*mean_errors.tolist()) + "\\\\")
-        elif args.dataset == 'kitti':
+        elif args.dataset == "kitti":
             print("\n  " + ("{:>8} | " * 7).format("abs_rel", "sq_rel", "rmse", "rmse_log", "a1", "a2", "a3"))
             print(("&{: 8.3f}  " * 7).format(*mean_errors.tolist()) + "\\\\")
 
