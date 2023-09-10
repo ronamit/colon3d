a34e2a21623af9a77dc05660d3e11adb355eb8b4
ronamit <ronamit@campus.technion.ac.il>
2023-09-10 07:32:37 +0000

    refact

--- colon3d/vid_mod/create_mod_vids.py
+++ colon3d/vid_mod/create_mod_vids.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 import numpy as np
 
-from colon3d.run_on_scene import SlamRunner
 from colon3d.util.general_util import ArgsHelpFormatter
+from colon3d.vid_mod.modify_video import VideoModifier
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -12,14 +12,28 @@ from colon3d.util.general_util import ArgsHelpFormatter
 def main():
     parser = argparse.ArgumentParser(formatter_class=ArgsHelpFormatter)
     parser.add_argument(
+        "--load_scene_path",
+        type=str,
+        default="/mnt/disk1/data/my_videos/Example_4",
+        help="Path to load scene",
+    )
+    parser.add_argument(
         "--base_save_path",
         type=str,
         default="/mnt/disk1/results/Mod_Vids",
         help="path to the save outputs",
     )
     args = parser.parse_args()
+    load_scene_path = Path(args.load_scene_path)
     base_save_path = Path(args.base_save_path)
     alg_fov_ratio = 0.8
+
+    video_modifier = VideoModifier(
+        load_scene_path=load_scene_path,
+        alg_fov_ratio=alg_fov_ratio,
+        n_frames_lim=0,
+        # verbose=False,
+    )
 
     # set the grid of scales to modify the video with
     seg_scales = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
@@ -27,23 +41,7 @@ def main():
     for scale in seg_scales:
         # Modify the video to have longer out-of-view segments
         mod_scene_path = base_save_path / f"Scale_{scale}".replace(".", "_")
-        mod_scene_results_path = mod_scene_path / "results"
-
-        # Run the algorithm on the modified video
-        slam_runner = SlamRunner(
-            scene_path=mod_scene_path,
-            save_path=mod_scene_results_path,
-            save_overwrite=True,
-            save_raw_outputs=False,
-            depth_maps_source="none",
-            egomotions_source="none",
-            depth_and_egomotion_method="none",
-            alg_fov_ratio=alg_fov_ratio,
-            n_frames_lim=0,
-            draw_interval=200,
-            verbose_print_interval=0,
-        )
-        slam_runner.run()
+        video_modifier.run(seg_scale=scale, save_path=mod_scene_path)
 
 
 # ---------------------------------------------------------------------------------------------------------------------
