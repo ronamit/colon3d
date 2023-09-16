bcee5f26bf43a9da069f22d29fe2c509ffde3862
ronamit <ronamit@campus.technion.ac.il>
2023-09-16 09:49:51 +0000

    d

--- colon3d/run_exp_col_nav.py
+++ colon3d/run_exp_col_nav.py
@@ -37,7 +37,7 @@ def main():
     parser.add_argument(
         "--results_base_path",
         type=str,
-        default="data/results/ColonNav",
+        default="data/results/ColonNav_v2",
         help="Base path for the results",
     )
     parser.add_argument(
